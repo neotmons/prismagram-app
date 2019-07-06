61060eabb208ef1b2b0759aba4d711b585701f8c
neotmons <neotmons@gmail.com>
2019-07-06 22:55:06 +0900

    #12.0 AuthHome

--- components/NavController.js
+++ components/NavController.js
@@ -5,8 +5,8 @@ import AuthNavigation from "../navigation/AuthNavigation";
 import MainNavigation from "../navigation/MainNavigation";
 
 export default () => {
-    //const isLoggedIn = useIsLoggedIn();
-    const isLoggedIn = true;
+    const isLoggedIn = useIsLoggedIn();
+    //const isLoggedIn = true;
 
     return (
         <View style={{ flex: 1 }}>
