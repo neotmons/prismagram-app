118535d9c0cfd6ecec59354ea3f959277c623397
neotmons <neotmons@gmail.com>
2019-07-07 09:23:52 +0900

    #12.5 Login part Three

--- apollo.js
+++ apollo.js
@@ -1,5 +1,6 @@
 const apolloClientOptions = {
-    uri: "http://localhost:4000"
+    //uri: "http://localhost:4000"
+    uri: "https://prismagram7-backend.herokuapp.com/"
 };
 
 export default apolloClientOptions;
--- components/AuthInput.js
+++ components/AuthInput.js
@@ -16,14 +16,28 @@ const TextInput = styled.TextInput`
     border-radius: 4px;
 `;
 
-const AuthInput = ({placeholder, value, keyboardType="default", autoCapitalize = "none", onChange}) => (
+const AuthInput = 
+    ({
+        placeholder, 
+        value, 
+        keyboardType="default", 
+        autoCapitalize = "none", 
+        onChange, 
+        returnKeyType="done",
+        onEndEditing = () => null,
+        autoCorrect = true
+    }) => (
     <Container>
         <TextInput 
             onChangeText={onChange}
             keyboardType={keyboardType} 
             placeholder={placeholder} 
+            value={value}
             autoCapitalize={autoCapitalize}
-            value={value} />
+            returnKeyType={returnKeyType}
+            autoCorrect={autoCorrect}
+            onEndEditing={onEndEditing}           
+             />
     </Container>
 );
 
@@ -32,7 +46,10 @@ AuthInput.propTypes = {
     value: PropTypes.string.isRequired,
     keyboardType: PropTypes.oneOf(["default", "number-pad", "decimal-pad", "numeric", "email-address", "phone-pad"]),
     autoCapitalize: PropTypes.oneOf(["none", "sentences", "words", "characters"]),
-    onChange: PropTypes.func.isRequired
+    onChange: PropTypes.func.isRequired,
+    returnKeyType: PropTypes.oneOf(["done", "go", "next", "search", "send"]),
+    onEndEditing: PropTypes.func,
+    autoCorrect: PropTypes.bool
 }
 
 export default AuthInput;
