666571274ab6f265b9a62870372bcea6d106897c
neotmons <neotmons@gmail.com>
2019-07-28 01:09:51 +0900

    #18.8 Uploading to Backend

--- apollo.js
+++ apollo.js
@@ -1,6 +1,6 @@
 const apolloClientOptions = {
-    //uri: "http://localhost:4000"
-    uri: "https://prismagram7-backend.herokuapp.com/"
+    uri: "http://localhost:4000"
+    //uri: "https://prismagram7-backend.herokuapp.com/"
 };
 
 export default apolloClientOptions;
--- screens/Photo/UploadPhoto.js
+++ screens/Photo/UploadPhoto.js
@@ -1,16 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { Image, ActivityIndicator, Alert } from "react-native";
 import styled from "styled-components";
+import useInput from "../../hooks/useInput";
+import styles from "../../styles";
+import constants from "../../constants";
 
 const View = styled.View`
-    justify-content: center;
-    align-items: center;
-    flex: 1;
+  flex: 1;
 `;
 
-const Text = styled.Text``;
+const Container = styled.View`
+  padding: 20px;
+  flex-direction: row;
+`;
 
-export default ({navigation}) => (
+const Form = styled.View`
+  justify-content: flex-start;
+`;
+
+const STextInput = styled.TextInput`
+  margin-bottom: 10px;
+  border: 0px solid ${styles.lightGreyColor};
+  border-bottom-width: 1px;
+  padding-bottom: 10px;
+  width: ${constants.width - 180};
+`;
+
+const Button = styled.TouchableOpacity`
+  background-color: ${props => props.theme.blueColor};
+  padding: 10px;
+  border-radius: 4px;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Text = styled.Text`
+  color: white;
+  font-weight: 600;
+`;
+
+export default ({ navigation }) => {
+  const [loading, setIsLoading] = useState(false);
+  const [fileUrl, setFileUrl] = useState("");
+  const photo = navigation.getParam("photo");
+  const captionInput = useInput("dfdf");
+  const locationInput = useInput("dfdfd");
+  const handleSubmit = async () => {
+    if (captionInput.value === "" || locationInput.value === "") {
+      Alert.alert("All fields are required");
+    }
+    const formData = new FormData();
+    const name = photo.filename;
+    const [, type] = name.split(".");
+    formData.append("file", {
+      name,
+      type: "image/jpeg",
+      uri: photo.uri
+    });
+    try {
+      const {
+        data: { path }
+      } = await axios.post("http://192.168.0.27:4000/api/upload", formData, {
+        headers: {
+          "content-type": "multipart/form-data"
+        }
+      });
+      setFileUrl(path);
+    } catch (e) {
+      Alert.alert("Cant upload", "Try later");
+    }
+  };
+  return (
     <View>
-        <Text>I should upload {navigation.getParam("photo").uri}</Text>
+      <Container>
+        <Image
+          source={{ uri: photo.uri }}
+          style={{ height: 80, width: 80, marginRight: 30 }}
+        />
+        <Form>
+          <STextInput
+            onChangeText={captionInput.onChange}
+            value={captionInput.value}
+            placeholder="Caption"
+            multiline={true}
+            placeholderTextColor={styles.darkGreyColor}
+          />
+          <STextInput
+            onChangeText={locationInput.onChange}
+            value={locationInput.value}
+            placeholder="Location"
+            multiline={true}
+            placeholderTextColor={styles.darkGreyColor}
+          />
+          <Button onPress={handleSubmit}>
+            {loading ? (
+              <ActivityIndicator color="white" />
+            ) : (
+              <Text>Upload </Text>
+            )}
+          </Button>
+        </Form>
+      </Container>
     </View>
-);
+  );
+};
